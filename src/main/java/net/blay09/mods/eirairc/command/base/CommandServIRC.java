322802d38b5b9b4d5e8b4f100f689d8ef7ff4280
Blay09 <blay09@hotmail.de>
2015-01-07 18:42:13 +0100

    Probably fix /help not working under certain circumstances.
    Fixes #119.

--- src/main/java/net/blay09/mods/eirairc/command/base/CommandServIRC.java
+++ src/main/java/net/blay09/mods/eirairc/command/base/CommandServIRC.java
@@ -20,7 +20,7 @@ public class CommandServIRC implements ICommand {
 
 	@Override
 	public int compareTo(Object o) {
-		return 0;
+		return getCommandName().compareTo(((ICommand) o).getCommandName());
 	}
 
 	@Override
--- src/main/java/net/blay09/mods/eirairc/command/base/IgnoreCommand.java
+++ src/main/java/net/blay09/mods/eirairc/command/base/IgnoreCommand.java
@@ -21,7 +21,7 @@ public class IgnoreCommand implements ICommand {
 	
 	@Override
 	public int compareTo(Object o) {
-		return 0;
+		return getCommandName().compareTo(((ICommand) o).getCommandName());
 	}
 
 	@Override
