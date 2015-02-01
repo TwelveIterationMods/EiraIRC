b46983e07821595df3349dc89056a6ae195d6e54
Blay09 <blay09@hotmail.de>
2015-02-01 23:23:49 +0100

    Only spam about unhandled message codes when debug mode is enabled.
    Fixes #133.

--- src/main/java/net/blay09/mods/eirairc/irc/IRCConnectionImpl.java
+++ src/main/java/net/blay09/mods/eirairc/irc/IRCConnectionImpl.java
@@ -339,7 +339,9 @@ public class IRCConnectionImpl implements Runnable, IRCConnection {
 		} else if(numeric <= 5 || numeric == 251 || numeric == 252 || numeric == 254 || numeric == 255 || numeric == 265 || numeric == 266 || numeric == 250 || numeric == 375) {
 			// ignore for now
 		} else {
-			System.out.println("Unhandled message code: " + msg.getCommand() + " (" + msg.argcount() + " arguments)");
+			if(GlobalConfig.debugMode) {
+				System.out.println("Unhandled message code: " + msg.getCommand() + " (" + msg.argcount() + " arguments)");
+			}
 		}
 		return true;
 	}
