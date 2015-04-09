b29b3c547a92d4c106d2063cbd6152083833c829
Blay09 <blay09@hotmail.de>
2015-04-09 18:10:25 +0200

    Display "twitchnotify" messages from Twitch Chat properly
    Fixes #168

--- src/main/java/net/blay09/mods/eirairc/handler/IRCEventHandler.java
+++ src/main/java/net/blay09/mods/eirairc/handler/IRCEventHandler.java
@@ -149,7 +149,9 @@ public class IRCEventHandler {
 			message = MessageFormat.filterLinks(message);
 		}
 		String format;
-		if(event.isNotice) {
+		if(event.connection.getHost().equals(Globals.TWITCH_SERVER) && event.sender != null && event.sender.getName().equals("twitchnotify")) {
+			format = "{MESSAGE}";
+		} else if(event.isNotice) {
 			format = botSettings.getMessageFormat().mcChannelNotice;
 		} else if(event.isEmote) {
 			format = botSettings.getMessageFormat().mcChannelEmote;
