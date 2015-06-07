62a16e4bac7bd0a551e52f8318e297458d71b80a
Blay09 <blay09@hotmail.de>
2015-06-07 23:09:15 +0200

    Fix scaling of image preview.
    Fixes #287

--- src/main/java/net/blay09/mods/eirairc/client/gui/base/image/GuiImage.java
+++ src/main/java/net/blay09/mods/eirairc/client/gui/base/image/GuiImage.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2014, Christopher "blay09" Baker
+// All rights reserved.
+
 package net.blay09.mods.eirairc.client.gui.base.image;
 
 import net.minecraft.client.renderer.Tessellator;
@@ -9,9 +12,10 @@ import org.lwjgl.opengl.GL11;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-// Copyright (c) 2014, Christopher "blay09" Baker
-// All rights reserved.
 public abstract class GuiImage extends AbstractTexture {
+
+	private float imageWidth;
+	private float imageHeight;
 
 	public void loadTexture() {
 		try {
@@ -26,6 +30,8 @@ public abstract class GuiImage extends AbstractTexture {
 		try {
 			BufferedImage bufferedImage = loadImage();
 			if(bufferedImage != null) {
+				imageWidth = bufferedImage.getWidth();
+				imageHeight = bufferedImage.getHeight();
 				TextureUtil.uploadTextureImage(getGlTextureId(), bufferedImage);
 			}
 		} catch (IOException e) {
@@ -36,13 +42,29 @@ public abstract class GuiImage extends AbstractTexture {
 	public abstract BufferedImage loadImage() throws IOException;
 
 	public void draw(int xPos, int yPos, int width, int height, float zLevel) {
+		float renderWidth = imageWidth;
+		float renderHeight = imageHeight;
+		float factor;
+		if(renderWidth > width) {
+			factor = width / renderWidth;
+			renderWidth *= factor;
+			renderHeight *= factor;
+		}
+		if(renderHeight > height) {
+			factor = height / renderHeight;
+			renderWidth *= factor;
+			renderHeight *= factor;
+		}
+		float renderX = xPos + width / 2 - renderWidth / 2;
+		float renderY = yPos + height / 2 - renderHeight / 2;
+
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, getGlTextureId());
 		Tessellator tessellator = Tessellator.instance;
 		tessellator.startDrawingQuads();
-		tessellator.addVertexWithUV(xPos, yPos + height, zLevel, 0, 1);
-		tessellator.addVertexWithUV(xPos + width, yPos + height, zLevel, 1, 1);
-		tessellator.addVertexWithUV(xPos + width, yPos, zLevel, 1, 0);
-		tessellator.addVertexWithUV(xPos, yPos, zLevel, 0, 0);
+		tessellator.addVertexWithUV(renderX, renderY + renderHeight, zLevel, 0, 1);
+		tessellator.addVertexWithUV(renderX + renderWidth, renderY + renderHeight, zLevel, 1, 1);
+		tessellator.addVertexWithUV(renderX + renderWidth, renderY, zLevel, 1, 0);
+		tessellator.addVertexWithUV(renderX, renderY, zLevel, 0, 0);
 		tessellator.draw();
 	}
 
