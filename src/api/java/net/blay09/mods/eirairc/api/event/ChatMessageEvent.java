351f239862169fe890b4db038fbaf1710ff50593
SoniEx2 <endermoneymod@gmail.com>
2015-07-06 16:08:33 -0300

    ChatMessageEvent is now an internal event.

--- src/api/java/net/blay09/mods/eirairc/api/event/ChatMessageEvent.java
+++ src/api/java/net/blay09/mods/eirairc/api/event/ChatMessageEvent.java
@@ -8,18 +8,25 @@ import net.minecraftforge.fml.common.eventhandler.Cancelable;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 @Cancelable
-@Deprecated
-// TODO remove this, replace it with the superior @HasResult system.
 public class ChatMessageEvent extends Event {
 
     public final ICommandSender target;
     public final IChatComponent component;
 
+    /**
+     * INTERNAL EVENT. YOU SHOULD NOT POST THIS YOURSELF.
+     * @param component the chat component to display
+     */
     public ChatMessageEvent(IChatComponent component) {
         this.target = null;
         this.component = component;
     }
 
+    /**
+     * INTERNAL EVENT. YOU SHOULD NOT POST THIS YOURSELF.
+     * @param target  the sender that issued the message
+     * @param component the chat component to display
+     */
     public ChatMessageEvent(ICommandSender target, IChatComponent component) {
         this.target = target;
         this.component = component;
